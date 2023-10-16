a415b612b8bec86392c2e2250d49d73d52926ba0
Parker <parkerphillips2003@gmail.com>
2023-10-15 19:47:49 -0700

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+import { useState, useRef, useEffect } from "react";
+import sound from "./Assets/laser.mp3";
+
+const laserAudio = new Audio(sound);
 
 function App() {
+  const [position, setPosition] = useState({ left: 50, top: 50 });
+
+  const handleMouseMove = (e: any) => {
+    setPosition({ left: e.clientX, top: e.clientY });
+  };
+
+  const laserClick = async () => {
+    laserAudio.play();
+  };
+
+  window.addEventListener("mousemove", handleMouseMove);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.gameWrapper} onClick={laserClick}>
+      <div
+        style={{ left: position.left, top: position.top }}
+        className={styles.box}
+      ></div>
+      <UFO position={position} />
     </div>
   );
 }
+
+const UFO = ({ position }: { position: { left: number; top: number } }) => {
+  const [rotation, setRotation] = useState(0);
+  const ufoRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ufoRef.current == null) return;
+    const ufo = ufoRef.current.getBoundingClientRect();
+    setRotation(
+      -Math.atan2((ufo.left - position.left) * 1.0, ufo.top - position.top) *
+        (180 / Math.PI)
+    );
+  }, [position]);
+
+  const handleClick = (e: any) => {
+    e.target.style.display = "none";
+  };
+  return (
+    <div
+      ref={ufoRef}
+      className={styles.ufo}
+      onClick={handleClick}
+      style={{ rotate: rotation + "deg" }}
+    ></div>
+  );
+};
 
 export default App;
