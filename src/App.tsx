2b506661776abaf55cc7e59d1b0689b8bb77a9bd
Parker <parkerphillips2003@gmail.com>
2023-10-20 20:12:19 -0700

    Added landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainGame from "./Pages/MainGame";
+import MainGame from "./Pages/maingame/MainGame";
+import { LandingPage } from "./Pages/landingpage/LandingPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path={"/"} element={<MainGame />} />
+        <Route path={"/"} element={<LandingPage />} />
+        <Route path={"/play"} element={<MainGame />} />
+        <Route path={"/*"} element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/landingpage/LandingPage.tsx
@@ -0,0 +1,69 @@
+import { SetStateAction, useEffect, useState } from "react";
+import styles from "./LandingPage.module.scss";
+import { useNavigate } from "react-router-dom";
+import bgMusic from "../../Assets/bgmusic.mp3";
+
+const bgAudio = new Audio(bgMusic);
+
+export const LandingPage = () => {
+  const navigate = useNavigate();
+  const [isChosen, setIsChosen] = useState(false);
+
+  useEffect(() => {
+    if (sessionStorage.getItem("volumeOn") == "true") {
+      bgAudio.volume = 0.1;
+      bgAudio.loop = true;
+      bgAudio.play();
+    }
+  }, [isChosen]);
+
+  window.onbeforeunload = () => {
+    sessionStorage.removeItem("volumeOn");
+  };
+
+  return (
+    <div className={styles.pageWrapper}>
+      {!sessionStorage.getItem("volumeOn") ? (
+        <VolumeModal setIsChosen={setIsChosen} />
+      ) : (
+        ""
+      )}
+      <h1 className={styles.title}>Galactic Shooter</h1>
+      <button className={styles.btn} onClick={() => navigate("/play")}>
+        Play
+      </button>
+      <button className={styles.btn}>Objective</button>
+      <button className={styles.btn}>Leaderboard</button>
+    </div>
+  );
+};
+
+type VolumeModalProps = {
+  setIsChosen: React.Dispatch<SetStateAction<boolean>>;
+};
+
+const VolumeModal = ({ setIsChosen }: VolumeModalProps) => {
+  const handleYesClick = () => {
+    sessionStorage.setItem("volumeOn", "true");
+    setIsChosen(true);
+  };
+
+  const handleNoClick = () => {
+    sessionStorage.setItem("volumeOn", "false");
+    setIsChosen(true);
+  };
+  return (
+    <div className={styles.volumeModal}>
+      <h1 className={styles.modalTitle}>This Web Application Uses Sound.</h1>
+      <h2 className={styles.modalSubTitle}>
+        Do You Allow Sound To Play On Your Computer?
+      </h2>
+      <button className={styles.btn} onClick={handleYesClick}>
+        Yes
+      </button>
+      <button className={styles.btn} onClick={handleNoClick}>
+        No
+      </button>
+    </div>
+  );
+};
